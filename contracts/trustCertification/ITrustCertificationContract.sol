7d8ff9f4317591fa019dc6e76f440ffa1ceb7f52
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2021-08-02 20:19:20 +0200

    Implement renew certificate

--- contracts/trustCertification/ITrustCertificationContract.sol
+++ contracts/trustCertification/ITrustCertificationContract.sol
@@ -5,9 +5,12 @@ pragma experimental ABIEncoderV2;
 interface ITrustCertificationContract { 
     
     function issueCertificate(address _recipientAddress, uint _certificateCourseId, uint _qualification) external returns(uint);
+    function renewCertificate(uint _id) external;
     function enableCertificate(uint _id) external;
     function disableCertificate(uint _id) external;
     function isCertificateValid(uint _id) external view returns (bool);
+    function getMyCertificatesAsRecipient() external view returns (CertificateRecord[] memory);
+    function getMyCertificatesAsIssuer() external view returns (CertificateRecord[] memory);
     
     // Data Structure
     struct CertificateRecord {
@@ -24,6 +27,7 @@ interface ITrustCertificationContract {
 
     // Events
     event OnNewCertificateGenerated(uint _id);
+    event OnCertificateRenewed(uint _id);
     event OnCertificateDeleted(uint _id);
     event OnCertificateEnabled(uint _id);
     event OnCertificateDisabled(uint _id);
