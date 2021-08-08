8ab432ff046d3f5b6b7b3160acf245940081ead1
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2021-08-08 16:00:13 +0200

    Initial configuration for Private Ethereum Network based on geth nodes

---
+++ eth_geth/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+cd /root/eth-net-intelligence-api
+perl -pi -e "s/XXX/$(hostname)/g" app.json
+pm2 start ./app.json
+sleep 3
+geth --datadir=~/.ethereum/devchain init "/root/genesis.json"
+geth --password /root/password account new
+sleep 3
+BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
+GETH_OPTS=${@/XXX/$BOOTSTRAP_IP}
+geth $GETH_OPTS
