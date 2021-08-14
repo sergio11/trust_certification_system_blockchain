5c1e68b636a89faeee5f3b4ef9b2fc0a07d21c21
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2021-08-14 12:34:29 +0200

    Initial Blockchain Gateway service configuration

---
+++ microservices/blockchain-gateway/src/main/java/com/dreamsoftware/blockchaingateway/BlockchainGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.dreamsoftware.blockchaingateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainGatewayApplication.class, args);
+	}
+
+}
