c2e28239c2cb6a663200215b54e74c584803d1b2
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2021-08-22 14:24:06 +0200

    [CONTRACTS] Using keccak256 to generate certificate course id instance of incrementing lastID

--- microservices/blockchain-gateway/src/main/resources/contracts/certificationCourse/ICertificationCourseContract.sol
+++ microservices/blockchain-gateway/src/main/resources/contracts/certificationCourse/ICertificationCourseContract.sol
@@ -5,20 +5,20 @@ pragma experimental ABIEncoderV2;
 
 interface ICertificationCourseContract { 
     
-    function addCertificationCourse(string memory _name, uint _costOfIssuingCertificate, uint _durationInHours) external returns (uint);
-    function addCertificationCourse(string memory _name, uint _costOfIssuingCertificate, uint _durationInHours, uint _expirationInDays, bool _canBeRenewed, uint _costOfRenewingCertificate) external returns (uint);
-    function removeCertificationCourse(uint _id) external;
-    function enableCertificationCourse(uint _id) external;
-    function disableCertificationCourse(uint _id) external;
-    function canBeIssued(uint _id) external view returns (bool);
-    function canBeRenewed(uint _id) external view returns (bool);
-    function isCertificationCourseExists(uint _id) external view returns (bool);
-    function getCostOfIssuingCertificate(uint _id) external view returns (uint);
-    function getCostOfRenewingCertificate(uint _id) external view returns (uint);
-    function getCertificateAuthorityForCourse(uint _id) external view returns (address);
-    function getDurationInHours(uint _id) external view returns (uint);
-    function getExpirationDate(uint _id) external view returns (uint);
-    function isYourOwner(uint _id, address _certificationAuthority) external view returns (bool);
+    function addCertificationCourse(string memory _name, uint _costOfIssuingCertificate, uint _durationInHours) external returns (string memory);
+    function addCertificationCourse(string memory _name, uint _costOfIssuingCertificate, uint _durationInHours, uint _expirationInDays, bool _canBeRenewed, uint _costOfRenewingCertificate) external returns (string memory);
+    function removeCertificationCourse(string memory _id) external;
+    function enableCertificationCourse(string memory _id) external;
+    function disableCertificationCourse(string memory _id) external;
+    function canBeIssued(string memory _id) external view returns (bool);
+    function canBeRenewed(string memory _id) external view returns (bool);
+    function isCertificationCourseExists(string memory _id) external view returns (bool);
+    function getCostOfIssuingCertificate(string memory _id) external view returns (uint);
+    function getCostOfRenewingCertificate(string memory _id) external view returns (uint);
+    function getCertificateAuthorityForCourse(string memory _id) external view returns (address);
+    function getDurationInHours(string memory _id) external view returns (uint);
+    function getExpirationDate(string memory _id) external view returns (uint);
+    function isYourOwner(string memory _id, address _certificationAuthority) external view returns (bool);
     
     // Data Structure
     struct CertificationCourseRecord {
@@ -34,10 +34,10 @@ interface ICertificationCourseContract {
     }
     
     // Events Definitions
-    event OnNewCertificationCourseCreated(uint _id);
-    event OnCertificationCourseRemoved(uint _id);
-    event OnCertificationCourseEnabled(uint _id);
-    event OnCertificationCourseDisabled(uint _id);
+    event OnNewCertificationCourseCreated(string _id);
+    event OnCertificationCourseRemoved(string _id);
+    event OnCertificationCourseEnabled(string _id);
+    event OnCertificationCourseDisabled(string  _id);
 }
 
 
