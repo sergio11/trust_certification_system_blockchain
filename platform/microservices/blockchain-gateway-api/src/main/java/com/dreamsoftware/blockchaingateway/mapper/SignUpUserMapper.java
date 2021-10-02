935f45b2a6f2ebe0e70c15550a8ba477017d375e
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2021-10-02 15:15:30 +0200

    [MICROSERVICES] Create endpoint for the registration of a certification authority.

--- platform/microservices/blockchain-gateway-api/src/main/java/com/dreamsoftware/blockchaingateway/mapper/SignUpUserMapper.java
+++ platform/microservices/blockchain-gateway-api/src/main/java/com/dreamsoftware/blockchaingateway/mapper/SignUpUserMapper.java
@@ -1,6 +1,10 @@
 package com.dreamsoftware.blockchaingateway.mapper;
 
+import com.dreamsoftware.blockchaingateway.persistence.nosql.entity.AuthorityEntity;
+import com.dreamsoftware.blockchaingateway.persistence.nosql.entity.AuthorityEnum;
 import com.dreamsoftware.blockchaingateway.persistence.nosql.entity.UserEntity;
+import com.dreamsoftware.blockchaingateway.persistence.nosql.entity.UserTypeEnum;
+import com.dreamsoftware.blockchaingateway.persistence.nosql.repository.AuthorityRepository;
 import com.dreamsoftware.blockchaingateway.web.dto.request.SignUpUserDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -8,6 +12,7 @@ import org.mapstruct.Mappings;
 import org.mapstruct.Named;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.Assert;
 
 /**
  *
@@ -23,6 +28,12 @@ public abstract class SignUpUserMapper {
     protected PasswordEncoder passwordEncoder;
 
     /**
+     * Authority Repository
+     */
+    @Autowired
+    protected AuthorityRepository authorityRepository;
+
+    /**
      * Sign Up User DTO to User Entity
      *
      * @param user
@@ -30,8 +41,36 @@ public abstract class SignUpUserMapper {
      */
     @Mappings({
         @Mapping(expression = "java(passwordEncoder.encode(user.getPasswordClear()))", target = "password"),
+        @Mapping(expression = "java(getUserAuthority(user.getType()))", target = "authority"),
         @Mapping(expression = "java(com.dreamsoftware.blockchaingateway.persistence.nosql.entity.UserTypeEnum.valueOf(user.getType()))", target = "type")
     })
     @Named("signUpUserDTOToUserEntity")
     public abstract UserEntity signUpUserDTOToUserEntity(SignUpUserDTO user);
+
+    /**
+     * Get User Authority
+     *
+     * @param userType
+     * @return
+     */
+    protected AuthorityEntity getUserAuthority(final String userType) {
+        Assert.notNull(userType, "User Type can not be null");
+        final UserTypeEnum userTypeEnum = UserTypeEnum.valueOf(userType);
+        final AuthorityEnum authorityTypeEnum;
+        switch (userTypeEnum) {
+            case CA:
+                authorityTypeEnum = AuthorityEnum.ROLE_CA;
+                break;
+            case STUDENT:
+                authorityTypeEnum = AuthorityEnum.ROLE_STUDENT;
+                break;
+            case ADMIN:
+                authorityTypeEnum = AuthorityEnum.ROLE_ADMIN;
+                break;
+            default:
+                authorityTypeEnum = null;
+                break;
+        }
+        return authorityRepository.findOneByType(authorityTypeEnum).orElse(null);
+    }
 }
--- platform/microservices/blockchain-gateway-common/src/main/java/com/dreamsoftware/blockchaingateway/persistence/nosql/repository/AuthorityRepository.java
+++ platform/microservices/blockchain-gateway-common/src/main/java/com/dreamsoftware/blockchaingateway/persistence/nosql/repository/AuthorityRepository.java
@@ -1,9 +1,11 @@
 package com.dreamsoftware.blockchaingateway.persistence.nosql.repository;
 
 import com.dreamsoftware.blockchaingateway.persistence.nosql.entity.AuthorityEntity;
+import com.dreamsoftware.blockchaingateway.persistence.nosql.entity.AuthorityEnum;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 /**
  *
@@ -12,4 +14,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AuthorityRepository extends MongoRepository<AuthorityEntity, ObjectId> {
 
+    /**
+     *
+     * @param type
+     * @return
+     */
+    Optional<AuthorityEntity> findOneByType(AuthorityEnum type);
 }
