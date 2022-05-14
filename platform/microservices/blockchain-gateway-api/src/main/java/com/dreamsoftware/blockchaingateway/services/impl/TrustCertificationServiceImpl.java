ac8df2ea89aab709f78cf72597caf08d2438323f
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2022-05-14 20:25:50 +0200

    [CERTIFICATION] Add Trusts Certification APIs

--- platform/microservices/blockchain-gateway-api/src/main/java/com/dreamsoftware/blockchaingateway/services/impl/TrustCertificationServiceImpl.java
+++ platform/microservices/blockchain-gateway-api/src/main/java/com/dreamsoftware/blockchaingateway/services/impl/TrustCertificationServiceImpl.java
@@ -1,10 +1,15 @@
 package com.dreamsoftware.blockchaingateway.services.impl;
 
+import com.dreamsoftware.blockchaingateway.mapper.CertificateIssuedMapper;
+import com.dreamsoftware.blockchaingateway.persistence.bc.repository.ITrustCertificationBlockchainRepository;
+import com.dreamsoftware.blockchaingateway.persistence.bc.repository.entity.CertificateIssuedEntity;
 import com.dreamsoftware.blockchaingateway.services.ITrustCertificationService;
+import com.dreamsoftware.blockchaingateway.web.dto.response.CertificateIssuedDTO;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 /**
  *
@@ -15,5 +20,38 @@ import org.springframework.stereotype.Service;
 public class TrustCertificationServiceImpl implements ITrustCertificationService {
 
     private static final Logger logger = LoggerFactory.getLogger(TrustCertificationServiceImpl.class);
+
+    private final CertificateIssuedMapper certificateIssuedMapper;
+    private final ITrustCertificationBlockchainRepository trustCertificationRepository;
+
+    /**
+     *
+     * @param ownerWallet
+     * @param certificationId
+     * @return
+     * @throws Throwable
+     */
+    @Override
+    public CertificateIssuedDTO enable(String ownerWallet, String certificationId) throws Throwable {
+        Assert.notNull(ownerWallet, "ownerWallet can not be null");
+        Assert.notNull(certificationId, "certificationId can not be null");
+        final CertificateIssuedEntity certificate = trustCertificationRepository.enable(ownerWallet, certificationId);
+        return certificateIssuedMapper.certificateIssuedEntityToCertificateIssued(certificate);
+    }
+
+    /**
+     *
+     * @param ownerWallet
+     * @param certificationId
+     * @return
+     * @throws Throwable
+     */
+    @Override
+    public CertificateIssuedDTO disable(String ownerWallet, String certificationId) throws Throwable {
+        Assert.notNull(ownerWallet, "ownerWallet can not be null");
+        Assert.notNull(certificationId, "certificationId can not be null");
+        final CertificateIssuedEntity certificate = trustCertificationRepository.disable(ownerWallet, certificationId);
+        return certificateIssuedMapper.certificateIssuedEntityToCertificateIssued(certificate);
+    }
 
 }
