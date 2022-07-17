b8441a257e2360bf5aa180fd66859a4c8d991efe
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2022-07-17 20:29:39 +0200

    [CONFIG] Create Spring Cloud Config Server / Discovery Service in order to provide a centralize service of configuration

--- platform/microservices/blockchain-gateway-processor/src/main/java/com/dreamsoftware/blockchaingateway/BlockchainGatewayProcessorApplication.java
+++ platform/microservices/blockchain-gateway-processor/src/main/java/com/dreamsoftware/blockchaingateway/BlockchainGatewayProcessorApplication.java
@@ -1,11 +1,13 @@
 package com.dreamsoftware.blockchaingateway;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableEncryptableProperties
 public class BlockchainGatewayProcessorApplication {
 
     public static void main(String[] args) {
