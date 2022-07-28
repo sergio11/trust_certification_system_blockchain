f24537e13ff6878cb84584c1b76df88c7294bf3f
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2022-07-28 12:22:48 +0200

    Refactoring of the event processor microservice architecture.

---
+++ platform/microservices/trust-certification-system-core/src/main/java/com/dreamsoftware/tcs/model/events/OnNewIssueCertificateRequestEvent.java
@@ -0,0 +1,33 @@
+package com.dreamsoftware.tcs.model.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author ssanchez
+ *
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OnNewIssueCertificateRequestEvent {
+
+    /**
+     * Student Wallet Hash
+     */
+    private String studentWalletHash;
+
+    /**
+     * Course Id
+     */
+    private String courseId;
+
+    /**
+     * Qualification
+     */
+    private Long qualification;
+}
---
+++ platform/microservices/trust-certification-system-course-events-processor/src/main/java/com/dreamsoftware/tcs/processor/CertificationCourseRegisteredProcessor.java
@@ -0,0 +1,44 @@
+package com.dreamsoftware.tcs.processor;
+
+import com.dreamsoftware.tcs.model.events.CertificationCourseRegisteredEvent;
+import com.dreamsoftware.tcs.persistence.nosql.entity.CertificationCourseEntity;
+import com.dreamsoftware.tcs.persistence.nosql.entity.CertificationCourseStateEnum;
+import com.dreamsoftware.tcs.persistence.nosql.entity.UserEntity;
+import com.dreamsoftware.tcs.persistence.nosql.repository.CertificationCourseRepository;
+import com.dreamsoftware.tcs.persistence.nosql.repository.UserRepository;
+import java.util.Date;
+import java.util.function.Consumer;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Certification Course Registered Processor
+ *
+ * @author ssanchez
+ */
+@Component("courseRegisteredProcessor")
+@RequiredArgsConstructor
+public class CertificationCourseRegisteredProcessor implements Consumer<CertificationCourseRegisteredEvent> {
+
+    private final Logger logger = LoggerFactory.getLogger(CertificationCourseRegisteredProcessor.class);
+
+    private final CertificationCourseRepository certificationCourseRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public void accept(CertificationCourseRegisteredEvent event) {
+        logger.debug("CertificationCourseRegisteredEvent CALLED!");
+        final UserEntity caUser = userRepository.findOneByWalletHash(event.getCaWalletHash()).orElseThrow(() -> new IllegalStateException("CA Wallet Hash not found"));
+        final CertificationCourseEntity certificationCourseEntity = CertificationCourseEntity
+                .builder()
+                .ca(caUser)
+                .createdAt(new Date())
+                .status(CertificationCourseStateEnum.ENABLED)
+                .courseId(event.getCertificationCourse().getId())
+                .build();
+        certificationCourseRepository.save(certificationCourseEntity);
+
+    }
+}
