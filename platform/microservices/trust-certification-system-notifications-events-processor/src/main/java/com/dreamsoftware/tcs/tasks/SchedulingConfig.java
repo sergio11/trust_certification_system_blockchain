2b58ae83a9b8b8b2e36418734bc2afdc9b7c4de1
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2022-09-09 21:20:21 +0200

    [CACHE] Implement Redis Cache System

--- platform/microservices/trust-certification-system-notifications-events-processor/src/main/java/com/dreamsoftware/tcs/tasks/SchedulingConfig.java
+++ platform/microservices/trust-certification-system-notifications-events-processor/src/main/java/com/dreamsoftware/tcs/tasks/SchedulingConfig.java
@@ -2,11 +2,11 @@ package com.dreamsoftware.tcs.tasks;
 
 import java.util.concurrent.Executor;
 import net.javacrumbs.shedlock.core.LockProvider;
-import net.javacrumbs.shedlock.provider.zookeeper.curator.ZookeeperCuratorLockProvider;
+import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
-import org.apache.curator.framework.CuratorFramework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
@@ -46,11 +46,11 @@ public class SchedulingConfig implements SchedulingConfigurer {
 
     /**
      *
-     * @param client
+     * @param connectionFactory
      * @return
      */
     @Bean
-    public LockProvider lockProvider(CuratorFramework client) {
-        return new ZookeeperCuratorLockProvider(client);
+    public LockProvider lockProvider(final RedisConnectionFactory connectionFactory) {
+        return new RedisLockProvider(connectionFactory, "Shedlock");
     }
 }
