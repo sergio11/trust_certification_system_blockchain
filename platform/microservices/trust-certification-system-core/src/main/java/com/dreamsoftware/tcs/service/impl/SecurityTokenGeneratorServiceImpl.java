927c4cbd31ee84f32bdcc2601b63ad47235ac26e
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2022-09-21 21:15:02 +0200

    [SECURITY] Improve the security of the token generation service

--- platform/microservices/trust-certification-system-core/src/main/java/com/dreamsoftware/tcs/service/impl/SecurityTokenGeneratorServiceImpl.java
+++ platform/microservices/trust-certification-system-core/src/main/java/com/dreamsoftware/tcs/service/impl/SecurityTokenGeneratorServiceImpl.java
@@ -4,6 +4,9 @@ import java.security.SecureRandom;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import com.dreamsoftware.tcs.service.ISecurityTokenGeneratorService;
+import com.google.common.hash.Hashing;
+import com.google.common.io.ByteSource;
+import java.io.IOException;
 
 /**
  *
@@ -14,11 +17,21 @@ public class SecurityTokenGeneratorServiceImpl implements ISecurityTokenGenerato
 
     protected static SecureRandom random = new SecureRandom();
 
+    /**
+     *
+     * @param id
+     * @return
+     */
     @Override
-    public String generateToken(String id) {
+    public String generateToken(final String id) {
         Assert.notNull(id, "Id can not be null");
         Assert.hasLength(id, "Id can not be empty");
-        long longToken = Math.abs(random.nextLong());
-        return (id.toLowerCase().trim().replace(" ", "_") + ":" + Long.toString(longToken, 16));
+        final String token = (id.toLowerCase().trim().replace(" ", "_") + ":" + Long.toString(Math.abs(random.nextLong()), 51));
+        try {
+            return ByteSource.wrap(token.getBytes())
+                    .hash(Hashing.sha256()).toString();
+        } catch (IOException ex) {
+            return token;
+        }
     }
 }
