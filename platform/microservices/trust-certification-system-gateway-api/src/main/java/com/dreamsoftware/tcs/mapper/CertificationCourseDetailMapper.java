0a990ffdb0284c3549b1ae5ea9d8986763fcc665
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2022-12-17 13:30:39 +0100

    Solve minor issues

--- platform/microservices/trust-certification-system-gateway-api/src/main/java/com/dreamsoftware/tcs/mapper/CertificationCourseDetailMapper.java
+++ platform/microservices/trust-certification-system-gateway-api/src/main/java/com/dreamsoftware/tcs/mapper/CertificationCourseDetailMapper.java
@@ -1,9 +1,14 @@
 package com.dreamsoftware.tcs.mapper;
 
+import com.dreamsoftware.tcs.persistence.nosql.entity.CertificationCourseEditionEntity;
 import com.dreamsoftware.tcs.persistence.nosql.entity.CertificationCourseEntity;
+import com.dreamsoftware.tcs.persistence.nosql.repository.CertificationCourseEditionRepository;
 import com.dreamsoftware.tcs.web.dto.response.CertificationCourseDetailDTO;
+import org.bson.types.ObjectId;
 import org.mapstruct.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 /**
  *
@@ -18,6 +23,9 @@ public abstract class CertificationCourseDetailMapper {
     @Autowired
     protected CertificationCourseEditionDetailMapper certificationCourseEditionDetailMapper;
 
+    @Autowired
+    protected CertificationCourseEditionRepository certificationCourseEditionRepository;
+
     /**
      *
      * @param entity
@@ -27,7 +35,7 @@ public abstract class CertificationCourseDetailMapper {
             @Mapping(expression = "java(entity.getId().toString())", target = "id"),
             @Mapping(expression = "java(entity.getStatus().name())", target = "status"),
             @Mapping(expression = "java(simpleCertificationAuthorityDetailMapper.entityToDTO(entity.getCa()))", target = "ca"),
-            @Mapping(expression = "java(certificationCourseEditionDetailMapper.entityToDTO(entity.getEditions()))", target = "editions")
+            @Mapping(expression = "java(certificationCourseEditionDetailMapper.entityToDTO(getCertificationCourseEditions(entity.getId())))", target = "editions")
     })
     @Named("entityToDTO")
     public abstract CertificationCourseDetailDTO entityToDTO(CertificationCourseEntity entity);
@@ -39,4 +47,13 @@ public abstract class CertificationCourseDetailMapper {
      */
     @IterableMapping(qualifiedByName = "entityToDTO")
     public abstract Iterable<CertificationCourseDetailDTO> entityListToDTOList(Iterable<CertificationCourseEntity> entity);
+
+    /**
+     *
+     * @param courseId
+     * @return
+     */
+    protected Iterable<CertificationCourseEditionEntity> getCertificationCourseEditions(final ObjectId courseId) {
+        return certificationCourseEditionRepository.findAllByCourse(courseId);
+    }
 }
